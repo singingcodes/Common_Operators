1dad5f5991ed30c064fb43df7e22aeff8b525f31
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-04-19 21:00:19 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Common_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int numerator = 12;
+            // int denominator = 3;
+            // int v = numerator / denominator;
+            // int result = v;
+            // Console.WriteLine(result);
+            // string myName = "sarah";
+            // string password = "1234";
+            // string inputName = Console.ReadLine();
+            // string inputPassword = Console.ReadLine();
+            // if(myName == inputName || password == inputPassword){
+            //     Console.WriteLine("true");
+            // }
+            // itinerary operator
+             int x = 20;
+            int y = 10;
+            Console.WriteLine(++x);
+            Console.WriteLine(--y);
+            // string result = x > y? "x is greater than y" : "x is less than y";
+            // string result = x > y? "x is greater than y" : "x is less than y";
+            // Console.WriteLine(result);
+            // // if (x > y){
+            //     Console.WriteLine("correct");
+            // }else {
+            //     Console.WriteLine("not correct");
+            // }
+            
+        }
+    }
+}
